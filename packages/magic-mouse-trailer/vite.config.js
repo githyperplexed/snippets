d03229c320d0c58ecbed97d41ae6a50153965f78
Cory Loeffelman <hyperplexed.email@gmail.com>
2026-01-23 15:23:16 -0600

    magic-mouse-trailer: added effect

---
+++ packages/magic-mouse-trailer/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: "script.js",
+        chunkFileNames: "script.js",
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name && assetInfo.name.endsWith(".css")) {
+            return "style.css";
+          }
+          return "[name][extname]";
+        },
+      },
+    },
+  },
+});
