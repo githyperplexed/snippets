966c74dbb7193546b0d7dbefcb1405b668ea5b2c
Cory Loeffelman <hyperplexed.email@gmail.com>
2026-05-05 11:37:32 -0500

    arm interactivity progress

--- packages/slay-the-spire/src/script.ts
+++ packages/slay-the-spire/src/script.ts
@@ -1,5 +1,18 @@
-const main = document.querySelector<HTMLElement>("main");
+const container = document.querySelector<HTMLDivElement>(".container")!;
 
-if (main) {
-	console.log("slay-the-spire ready");
+const cards = document.querySelectorAll<HTMLDivElement>(".card");
+
+const setCardPosition = (row: string = "", column: string = "") => {
+	container.style.setProperty("--card-row", row);
+	container.style.setProperty("--card-column", column);
+}
+
+for (const card of cards) {
+	card.onpointerenter = () => {
+		setCardPosition(card.dataset.row, card.dataset.column);
+	}
+
+	card.onpointerleave = () => {
+		setCardPosition();
+	}
 }
