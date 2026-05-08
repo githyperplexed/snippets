cb08c990f0a82bd4f816f2b6af344dbebd083322
Cory Loeffelman <hyperplexed.email@gmail.com>
2026-05-08 11:33:53 -0500

    slay-the-spire(style): subtle opacity and pattern size improvements

--- packages/slay-the-spire/src/lib/components/app.svelte
+++ packages/slay-the-spire/src/lib/components/app.svelte
@@ -5,7 +5,7 @@
 
 <main
 	class="pattern-grid flex min-h-svh flex-col items-center justify-center overflow-hidden"
-	style="--pattern-op: 0.04; --pattern-sz: 10rem 10rem;"
+	style="--pattern-op: 0.06; --pattern-sz: 20rem 20rem;"
 >
 	<Container />
 	<Deck />
--- packages/slay-the-spire/src/lib/components/container.svelte
+++ packages/slay-the-spire/src/lib/components/container.svelte
@@ -7,7 +7,7 @@
 
 <div
 	class="card-container pattern-dots relative grid grid-cols-4 overflow-hidden bg-black"
-	style="--pattern-op: 0.15; --pattern-sz: 3em 3em;"
+	style="--pattern-op: 0.1; --pattern-sz: 5em 5em;"
 >
 	{#each cards as card (card.index)}
 		<Card data={card} />
@@ -21,7 +21,7 @@
 		font-size: clamp(0.225rem, 0.6vw, 0.72rem);
 		gap: 3em;
 		padding: 3em;
-		border: max(1px, 0.1em) solid oklch(0.5 0 0);
+		border: max(1px, 0.1em) solid oklch(0.3 0 0);
 		border-radius: 1.8em;
 	}
 </style>
